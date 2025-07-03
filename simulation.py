50309ad8c35ba3ed79c7ac1f6b7a24258d5ed5f3
RF-UoA <al884959@adelaide.edu.au>
2025-07-03 17:32:47 +0930

    Refactored to use object oriented design.

--- simulation.py
+++ simulation.py
@@ -2,51 +2,74 @@ import pyvista
 import math
 import numpy as np
 
-radius = 1737400  # Radius of the Moon in meters
+class LunarSimulation:
+    def __init__(self):
+        self.radius = 1737400  # Radius of the Moon in meters
+        self.moon_sphere = None
+        self.star_sphere = None
 
-moon_sphere = pyvista.Sphere(
-    radius=radius,
-    theta_resolution=120,
-    phi_resolution=120,
-    start_theta=270.001, end_theta=270
-)
+    def create_moon(self):
+        self.moon_sphere = pyvista.Sphere(
+            radius=self.radius,
+            theta_resolution=120,
+            phi_resolution=120,
+            start_theta=270.001, end_theta=270
+        )
 
-star_sphere = pyvista.Sphere(
-    radius=radius * 100,  # Large sphere for stars
-    theta_resolution=120,
-    phi_resolution=120,
-    start_theta=270.00001, end_theta=270
-)
+    def create_stars(self):
+        self.star_sphere = pyvista.Sphere(
+            radius=self.radius * 100,  # Large sphere for stars
+            theta_resolution=120,
+            phi_resolution=120,
+            start_theta=270.00001, end_theta=270
+        )
 
-# Normalize to unit sphere for UV mapping
-moon_points = moon_sphere.points / radius
+    def normalize_points(self, sphere, radius):
+        points = sphere.points / radius
+        sphere.active_texture_coordinates = np.zeros((sphere.points.shape[0], 2))
+        sphere.active_texture_coordinates[:, 0] = 0.5 + np.arctan2(-points[:, 0], points[:, 1]) / (2 * math.pi)
+        sphere.active_texture_coordinates[:, 1] = 0.5 + np.arcsin(points[:, 2]) / math.pi
 
-star_points = star_sphere.points / (radius * 100)
+    def load_textures(self):
+        moon_texture = pyvista.read_texture("8k_moon.jpg")
+        stars_texture = pyvista.read_texture("8k_stars.jpg")
+        return moon_texture, stars_texture
+    
+    def setup_plotter(self, moon_texture, stars_texture):
+        pl = pyvista.Plotter()
 
-moon_sphere.active_texture_coordinates = np.zeros((moon_sphere.points.shape[0], 2))
-moon_sphere.active_texture_coordinates[:, 0] = 0.5 + np.arctan2(-moon_points[:, 0], moon_points[:, 1]) / (2 * math.pi)
-moon_sphere.active_texture_coordinates[:, 1] = 0.5 + np.arcsin(moon_points[:, 2]) / math.pi
+        pl.add_mesh(self.moon_sphere, texture=moon_texture, smooth_shading=False)
+        pl.add_mesh(self.star_sphere, texture=stars_texture, smooth_shading=False)
 
-star_sphere.active_texture_coordinates = np.zeros((star_sphere.points.shape[0], 2))
-star_sphere.active_texture_coordinates[:, 0] = 0.5 + np.arctan2(-star_points[:, 0], star_points[:, 1]) / (2 * math.pi)
-star_sphere.active_texture_coordinates[:, 1] = 0.5 + np.arcsin(star_points[:, 2]) / math.pi
+        # Set starting camera position at equator of the Moon
+        pl.camera_position = [(0, self.radius * 4, 0), (0, 0, 0), (0, 0, 1)]
 
-# Load textures
-moon = pyvista.read_texture("8k_moon.jpg")
-stars = pyvista.read_texture("8k_stars.jpg")
+        # Set max zoom level
+        pl.camera.zoom(1.5)
 
-pl = pyvista.Plotter()
+        # Set background color to black
+        pl.set_background("black")
 
-pl.add_mesh(moon_sphere, texture=moon, smooth_shading=False)
-pl.add_mesh(star_sphere, texture=stars, smooth_shading=False)
+        return pl
 
-# Set starting camera position at equator of the Moon
-pl.camera_position = [(0, radius * 4, 0), (0, 0, 0), (0, 0, 1)]
+class LunarSimulator():
+    def __init__(self):
+        self.simulation = LunarSimulation()
+        self.moon_radius = 1737400  # Radius of the Moon in meters
 
-# Set max zoom level
-pl.camera.zoom(1.5)
+    def run(self):
 
-# Set background color to black
-pl.set_background("black")
+        # Create the Moon and stars
+        self.simulation.create_moon()
+        self.simulation.create_stars()
 
-pl.show()
+        # Normalize points and set up textures
+        self.simulation.normalize_points(self.simulation.moon_sphere, self.moon_radius)
+        self.simulation.normalize_points(self.simulation.star_sphere, self.moon_radius * 100)
+        moon_texture, stars_texture = self.simulation.load_textures()
+
+        # Set up the plotter with textures and camera
+        pl = self.simulation.setup_plotter(moon_texture, stars_texture)
+        pl.show()
+
+LunarSimulator().run()
